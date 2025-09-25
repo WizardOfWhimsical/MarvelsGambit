9c70bfdb1e2ee6fbe3f08c66667159b2b7c9f141
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-09-24 22:00:31 -0400

    added .env config

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import config from "#config";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
