5742f38d41d45d3285228a31ec5c750a34cc5e2e
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-09-27 10:44:09 -0400

    setting up fetch call to marvel

--- server.js
+++ server.js
@@ -14,9 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 app.get("/test", (req, res) => {
-   //will be the start fo the flow
+   //will build routes based off char, comics, series, stories, events
+   const ts = new Date().getTime();
+   const publicKey = process.env.PUBLIC_KEY;
+   const privateKey = process.env.PRIVATE_KEY;
+   const hash = md5(ts + privateKey + publicKey)
    console.log("Root endpoint hit");
-   res.json({message:"Hello from the server!"});
+fetch(`https://gateway.marvel.com/v1/public/characters?name=Gambit&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+.then(response => {response.json()})
+.then(data =>{} )
 });
 
 app.listen(config.port, () => {
