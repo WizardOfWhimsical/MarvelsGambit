8e6238bca055e94f7af3e71a049d6fc6a186d82a
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-09-27 12:10:27 -0400

    forgot to change server static serving file path

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public"));
+app.use(express.static("./root"));
 
 const ts = new Date().getTime();
    const publicKey = process.env.PUBLIC_KEY;
