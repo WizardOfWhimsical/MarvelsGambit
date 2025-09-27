f9e01660e87d91b27bdf01404e45dce1a99eb216
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-09-27 11:34:52 -0400

    console.log now returns array of data asked for at this point

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,6 @@ btn.addEventListener('click', () => {
 
     fetch("/test")
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data.data.results))
     .catch(err => console.error('Error fetching from server:', err));
 });
