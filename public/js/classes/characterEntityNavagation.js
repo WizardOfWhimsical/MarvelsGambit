b05c379de234e86aff1068a4801203bc13dfb891
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-10-11 16:02:19 -0400

    cleaned all comments and unnessary code

--- public/js/classes/characterEntityNavagation.js
+++ public/js/classes/characterEntityNavagation.js
@@ -3,26 +3,25 @@ class CharacterEntityNavigation {
     this.seriesURI = dataObject.data.results[0].series.collectionURI;
     this.eventsURI = dataObject.data.results[0].events.collectionURI;
   }
-  createElement(){
-    return document.createElement("a");
-  }
-  createEnityNavigation(){
-    const series = this.createElement();
-    const events = this.createElement();
-    return {series, events};
-  }
   setNavigationAttributes(){
-    const {series, events} = this.createEnityNavigation();
+    const series = document.createElement("a");
+    const events = document.createElement("a");
+
+
+
     series.setAttribute("data-uri", this.seriesURI);
     series.textContent = "Series";
+
     events.setAttribute("data-uri", this.eventsURI);
     events.textContent = "Events";
+
     return {series, events};
   }
   setNavigation(container){
     try{    
       const {series, events} = this.setNavigationAttributes();
-    container.append(events, series)
+
+      container.append(events, series)
     }catch{err => console.error("Navigation Error: ", err)}
   }
 }
